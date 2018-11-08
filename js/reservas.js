d1fc90f2c7ba4dcdbef0c5811ae1f9ec11f1d794
germancardenasm <germancardenas@gmail.com>
2018-11-08 00:01:17 -0500

    Guia tres en proceso

---
+++ js/reservas.js
@@ -0,0 +1,6 @@
+var reserva = function(horario,cantidadDePersonas,precioPorPersona,codigoDeDescuento){
+    this.horario= horario;
+    this.cantidadDePersonas= cantidadDePersonas;
+    this.precioPorPersona= precioPorPersona;
+    this.codigoDeDescuento= codigoDeDescuento
+}
